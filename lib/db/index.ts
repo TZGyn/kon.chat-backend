2882630581ab5254d09094dd896a262698ef66d8
TZGyn <ca538901d@mozmail.com>
2025-04-13 15:06:10 +0800

    use pg instead of bun sql (not stable)

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,25 @@
-import { drizzle } from 'drizzle-orm/bun-sql'
+// import { drizzle } from 'drizzle-orm/bun-sql'
+import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
+import * as pg from 'pg'
+const { Pool } = pg
 
-const client = new Bun.SQL(Bun.env.DATABASE_URL!)
+const pool = new Pool({
+	connectionString: Bun.env.DATABASE_URL!,
+})
 
-export const db = drizzle(client, { schema })
+// const client = new Bun.SQL({
+// 	url: Bun.env.DATABASE_URL!,
+// 	max: 15, // Maximum connections in pool
+// 	idleTimeout: 30, // Close idle connections after 30s
+// 	maxLifetime: 0, // Connection lifetime in seconds (0 = forever)
+// 	connectionTimeout: 30, // Timeout when establishing new connections
+// 	onconnect: (client): void => {
+// 		console.log('sql connected')
+// 	},
+// 	onclose: (client): void => {
+// 		console.log('sql closed')
+// 	},
+// })
+
+export const db = drizzle(pool, { schema })
